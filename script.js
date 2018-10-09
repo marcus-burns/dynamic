9a9bcb369bef17e4da7313d1eae69aeb7940ee2e
marcus-burns <marcus.burns@dyson.com>
2018-10-09 09:20:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,52 +12,65 @@ Banner.animate = function(){
     
 	timeline.timeScale( 1 );
     
-    //timeline.to('#picture1', 0.0, {scale:0.25, x:-560, y:-373});             
-    //timeline.to('#picture1', 0.0, {autoAlpha:1});    
-   
+    // initial params //
+    
+    
+    timeline.to('#footer', 0.0, {autoAlpha:1});
+    
+    timeline.to('#ss', 3, {y:-5330, ease:SteppedEase.config(26), repeat:0}, 'spriteseq'); 
+    
+    //timeline.to('#ssfade', 0.5, {autoAlpha:1, delay:2.5}, 'spriteseq'); 
+    
+     
+    
+    //timeline.to('#line', 0.5, {autoAlpha:0}, "e");
+    //
+    timeline.to('#ss', 0.0, {autoAlpha:0}); 
+    timeline.to('#topper', 0.0, {autoAlpha:1});
+    
+    
+    timeline.to('#pic1', 0.5, {autoAlpha:1}, "a");
+    timeline.to('#pic1', 2.0, {scale:1.05}, "a");
+    timeline.to('#t1', 0.75, {autoAlpha:1}, "a");
+    timeline.to('#pic1', 0.15, {autoAlpha:0, delay:1.85}, "a");
+    
+    
+    timeline.to('#pic2', 0.5, {autoAlpha:1}, "b");
+    timeline.to('#pic2', 2.0, {scale:1.05}, "b");
+    timeline.to('#pic2', 0.15, {autoAlpha:0, delay:1.85}, "b");
+    
+    
+    timeline.to('#pic3', 0.5, {autoAlpha:1}, "c");
+    timeline.to('#pic3', 2.0, {scale:1.05}, "c");
+    
+    
+    timeline.to('#white', 0.5, {autoAlpha:1}); 
+    
+    timeline.to('#pic1', 0.0, {autoAlpha:0}, "e");
+    timeline.to('#pic2', 0.0, {autoAlpha:0}, "e");
+    timeline.to('#pic3', 0.0, {autoAlpha:0}, "e");
+    timeline.to('#t1', 0.0, {autoAlpha:0}, "e");
+    
+    timeline.to('#topper', 0.0, {autoAlpha:0}, "e");
+    
     
     timeline.set({},{},"+=0.1");
-      
     
     
-    timeline.to('#pink', 0.0, {top:-50});
     
-    timeline.to('#pink', 0.0, {opacity:1});
+    timeline.to('#endimg', 0.75, {autoAlpha:1, ease: Power4.easeOut, scale:1.0, transformOrigin: "center center"}, "h");
+    timeline.to('#logo', 0.75, {autoAlpha:1}, "h");
     
-    timeline.to('#pink', 0.5, {top:0});
-    
-    timeline.to('#t1', 0.5, {opacity:1});
     
     timeline.set({},{},"+=0.25");
     
-    timeline.to('#t2', 0.5, {opacity:1});
+    timeline.to('#txt1', 0.75, {autoAlpha:1});
+    timeline.to('#txt2', 0.75, {autoAlpha:1, delay:0.2});
     
     timeline.set({},{},"+=0.25");
+    timeline.to('#cta', 0.75, {autoAlpha:1});
     
-    timeline.to('#i1', 0.5, {opacity:1}, "a");
-    timeline.to('#i1', 2.0, {scale:1.1, transformOrigin:"50px"}, "a");
-    
-    //timeline.set({},{},"+=0.5");
-    
-    timeline.to('#i2', 0.5, {opacity:1}, "b");
-    timeline.to('#i2', 2.0, {scale:1.1, transformOrigin:"50px"}, "b");
-    
-    timeline.to('#i1', 0.0, {opacity:0});
-    timeline.to('#logo', 0.0, {top:-11});
-    timeline.to('#endimg', 0.0, {opacity:1});
-    timeline.to('#cta', 0.0, {autoAlpha:1});
-    
-    timeline.to('#i2', 0.5, {opacity:0}, "c");
-    timeline.to('#t1', 0.5, {opacity:0}, "c");
-    timeline.to('#t2', 0.5, {opacity:0}, "c");
-    timeline.to('#pink', 0.5, {top:50}, "c");
-    
-    timeline.set({},{},"+=0.25");
-    
-    timeline.to('#endtxt', 0.5, {opacity:1});
-    
-    
-    
-    //timeline.seek(5.85);  
+
+    //timeline.seek(8);  
 	console.log(timeline.duration());	
 }
