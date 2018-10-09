f834b5c0add95847b7623ae188496d3ea3e40891
marcus-burns <marcus.burns@dyson.com>
2018-10-09 16:14:43 +0100

    Add files via upload

--- iframe/script.js
+++ iframe/script.js
@@ -14,8 +14,19 @@ Banner.animate = function(){
        
     timeline.to('#ss1', 0, {autoAlpha:1}); 
     
-    timeline.to('#ss1', 11, {y:-7950, ease:SteppedEase.config(159), repeat:0}); 
+    timeline.to('#ss', 0, {autoAlpha:0}); 
+    
+    timeline.to('#ss1', 9, {y:-6550, ease:SteppedEase.config(131), repeat:0}); 
         
+    timeline.to('#ss1', 0.5, {autoAlpha:0}); 
+    
+    timeline.to('#cta', 0.666, {x:-130, ease: Power1.easeOut}, "a"); 
+    
+    timeline.to('#logo', 0.666, {x:-130, ease: Power1.easeOut}, "a"); 
+    
+    timeline.to('#text1', 0.5, {autoAlpha:1, delay:0.5}, "a");
+    
+    
     //timeline.seek(5.85);  
 	console.log(timeline.duration());	
 }
